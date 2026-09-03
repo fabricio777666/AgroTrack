85c3e0594976dfa9242d8f081d1b1e84ec5c6059
Eduardo Fernandes Silva <2024105070072@aluno.ifro.edu.br>
2026-09-03 10:32:06 -0400

    corrigi os meus erros

--- app.py
+++ app.py
@@ -1,31 +1,29 @@
-from httpx import request
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Lista para armazenar o maquinário cadastrado em memória
+maquinas = []
+
 @app.route('/')
 def index():
-    return "<h1> Sistema AgroTrack Rodando!</h1><p>A estrutura base foi criada com sucesso.</p>"
+    return render_template('index.html', maquinas=maquinas)
+
+@app.route('/cadastrar-produto')
+def cadastrar_produto():
+    return render_template('produto.html')
+
+@app.route('/relatorio')
+def relatorio():
+    return render_template('relatorio.html', maquinas=maquinas)
 
 @app.route('/cadastrar', methods=['GET', 'POST'])
 def cadastrar():
     if request.method == 'POST':
         nome_maquina = request.form['nome']
+        maquinas.append({'nome': nome_maquina})
         return f"Sucesso! A máquina '{nome_maquina}' foi cadastrada."
-    return render_template('cadastro.html')
+    return render_template('cadastrar.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    @app.route('/registrar', methods=['GET', 'POST'])
-    def registrar():
-        if request.method == 'POST':
-            maquina = request.form['maquina']
-            data = request.form['data']
-            horas = request.form['horas']
-            combustivel = request.form['combustivel']
-
-            return "Uso registrado com sucesso!"
-
-        return render_template('registrar_uso.html')
